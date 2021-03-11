99e40744e3f5e9a5d49e2bc48630e38c34b551ab
gvdp <vdputteglenn@gmail.com>
2021-03-11 22:27:42 +0100

    run backstop test in index

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test setup")
+const backstop = require('backstopjs')
+const customConfig = require('backstop.json')
+
+if (process.env.CI === 'true') {
+	customConfig.dockerCommandTemplate =
+		'docker run --rm -i --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}'
+}
+
+console.log('Running backstop with config', customConfig)
+
+backstop('test', { config: customConfig, docker: true }).catch((err) => {
+	console.error('Backstop test failed with ', err)
+	if (process.env.CI === 'true') {
+		process.exit(1)
+	}
+})
