2c7e85786d42f7fb09394746f3523a07a7c7544f
gvdp <vdputteglenn@gmail.com>
2021-03-24 21:24:30 +0100

    set cwd

--- action/index.js
+++ action/index.js
@@ -34,7 +34,7 @@ console.log('config parsed')
 console.log('Running backstop with config', configFile)
 
 async function runTest() {
-	return exec.exec('yarn test', [], {}).catch((err) => {
+	return exec.exec('yarn test', [], {cwd: '/action'}).catch((err) => {
 		console.error('Backstop test failing with ', err)
 		if (process.env.CI === 'true') {
 			//todo: make this mark the build as failed
