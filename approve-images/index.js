1dda30b50ad397af51d7efc6c5926c64048f2c09
gvdp <vdputteglenn@gmail.com>
2021-03-30 22:27:29 +0200

    download artifact to root

--- approve-images/index.js
+++ approve-images/index.js
@@ -104,7 +104,7 @@ async function downloadArtifact() {
 		const artifactUrl = resp.headers.location
 
 		console.log('Found url', artifactUrl)
-		const fileName = 'approve-images/backstop_report.zip'
+		const fileName = 'backstop_report.zip'
 		const downloadStream = got.stream(artifactUrl)
 		const fileWriterStream = fs.createWriteStream(fileName)
 		console.log(`Downloading ${artifactUrl}`)
@@ -117,8 +117,7 @@ async function downloadArtifact() {
 
 		console.log('Done?')
 		// await exec.exec('wget', [artifactUrl, '-O', './approve-images'])
-		await exec.exec('ls', ['approve-images', '-al'])
-		await exec.exec('unzip', [fileName, '-d', 'approve-images/backstop_data'])
+		await exec.exec('unzip', [fileName, '-d', 'backstop_data'])
 
 
 		await exec.exec('ls', ['approve-images/backstop_data', '-al'])
