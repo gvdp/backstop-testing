517746f7d4ed279db9fe2ee0bdec8b8ef150a63e
gvdp <vdputteglenn@gmail.com>
2021-03-30 21:34:59 +0200

    git commands

--- approve-images/index.js
+++ approve-images/index.js
@@ -122,7 +122,10 @@ async function downloadArtifact() {
 async function commitResult() {
 	console.log('committing')
 	await exec.exec('git', ['status'])
-	await exec.exec('git', ['add', '.'])
+	await exec.exec('git', ['add', 'approve-images/backstop_data'])
+	await exec.exec('git', ['status'])
+	await exec.exec('git', ['config', '--global', 'user.email', 'ikke@hotmail.com'])
+	await exec.exec('git', ['config', '--global', 'user.name', 'github'])
 	await exec.exec('git', ['commit', '-m', 'what up'])
 	await exec.exec('git', ['push'])
 }
